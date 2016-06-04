42ac1659c37e7872a5283459d0d012d5c8968fd7
tibyte <ttl74283@gmail.com>
2016-06-04 23:58:04 +0900

    apply sample code

---
+++ test.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+
+$("#list1 li").on("click", function(e){
+  $("#sel1").text($(this).text());
+});
+
+
+
+});
--- tgridview.js
+++ tgridview.js
@@ -1,44 +1,10 @@
-$(document).ready(function(){
+var TGridView = function()
+{
+  this.init();
+}
 
-  $("#list1 li").on("click", function(e){
-    $("#sel1").text($(this).text());
-  });
+TGridView.prototype = {
 
-  var table = $("#subject");
-  for(var i=0; i<800; i++) {
-    var id = "row"+i;
-    var html = `<div class="panel panel-default sbj-row" id="`+id+`">`;
-    for(var j=0; j<6; j++) {
-      html += `
-      <div class="panel panel-default sbj-cell">
-        <div class="panel-body sbj-context">`;
-      html += id + " col" + j;
-      html += `
-        </div>
-      </div>`;
-    }
-    html += "</div>"
-    table.append(html);
-  }
+}
 
-/*
-  for(var i=0; i<800; i++)
-    $("#subject").append(`
-      <div class="panel panel-default" id="row" style="margin:0px">
-
-          <div class="panel panel-default sbj-cell">
-            <div class="panel-body sbj-context">
-              cewwwwwwwwwwwwww
-            </div>
-          </div>
-          <div class="panel panel-default sbj-cell">
-            <div class="panel-body sbj-context">
-              cewwwwwwwwwwwwww
-            </div>
-          </div>
-
-      </div>
-    `);
-*/
-
-});
+TGridView.prototype.constructor = TGridView;
