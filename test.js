950fdb8d18f8d7e5a2d69e094b7012b71104e931
tibyte <ttl74283@gmail.com>
2016-06-05 03:26:07 +0900

    create TGridView class

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
 
-
 $("#list1 li").on("click", function(e){
   $("#sel1").text($(this).text());
 });
 
+var grid = new TGridView("sbj-list", ["code", "title", "cap"]);
 
 
 });
--- tgridview.js
+++ tgridview.js
@@ -1,10 +1,33 @@
-var TGridView = function()
+var TGridView = function(id, attrs)
 {
-  this.init();
+  this.init(id, attrs);
 }
 
 TGridView.prototype = {
+  init : function(id, attrs)
+  {
+    var html = "";
+    var attrCount = attrs.length;
 
+    html += "<table class=\"table table-hover table-frame noselect\"><thead><tr>";
+    for(var i=0; i<attrCount; i++) {
+      html += "<th>" + attrs[i] + "</th>";
+    }
+    html += "</tr></thead><tbody></tbody></table>";
+
+    $("#"+id).append(html);
+    html = "";
+    var start = performance.now();
+    for(var i=0; i<800; i++) {
+      html += "<tr id=row"+i+">";
+      for(var j=0; j<attrCount; j++) {
+        html += "<td>Cell</td>";
+      }
+      html += "</tr>";
+    }
+    $("#"+id+" tbody").append(html);
+    console.log(performance.now()-start);
+  }
 }
 
 TGridView.prototype.constructor = TGridView;
