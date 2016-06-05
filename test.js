3ae4f31a1ee489ca52cbd0a448150990ec1b1e4e
tibyte <ttl74283@gmail.com>
2016-06-05 23:57:28 +0900

    Add addRow() add selecter to on()

--- test.js
+++ test.js
@@ -7,5 +7,11 @@ $("#list1 li").on("click", function(e){
 
 var grid = new TGridView("sbj-list", ["과목코드", "과목명", "분반"]);
 
+for(var i=0; i<20; i++) {
+  grid.addRow([i+",1",i+",2",i+",3"]);
+}
+
+
+
 
 });
--- tgridview.js
+++ tgridview.js
@@ -1,34 +1,28 @@
 var TGridView = function(id, attrs)
 {
   this.selected = null;
-  this.init(id, attrs);
+  this.id = id;
+  this.attrs = attrs;
+  this.attrCount = attrs.length;
+
+  this.init();
 }
 
 TGridView.prototype = {
-  init : function(id, attrs)
+  init : function()
   {
     var html = "";
-    var attrCount = attrs.length;
 
+    //draw table header
     html += "<table class=\"table table-hover table-frame noselect\"><thead><tr>";
-    for(var i=0; i<attrCount; i++) {
-      html += "<th>" + attrs[i] + "</th>";
+    for(var i=0; i<this.attrCount; i++) {
+      html += "<th>" + this.attrs[i] + "</th>";
     }
     html += "</tr></thead><tbody></tbody></table>";
+    $("#"+this.id).append(html);
 
-    $("#"+id).append(html);
-    html = "";
 
-    for(var i=0; i<20; i++) {
-      html += "<tr class=\"row-body\">";
-      for(var j=0; j<attrCount; j++) {
-        html += "<td>Cell</td>";
-      }
-      html += "</tr>";
-    }
-    $("#"+id+" tbody").append(html);
-
-    $("#"+id+" .row-body").on("click", {ctx:this}, this.onRowSelected);
+    $("#"+this.id).on("click", ".row-body", {ctx:this}, this.onRowSelected);
   },
 
   onRowSelected : function(e)
@@ -37,6 +31,17 @@ TGridView.prototype = {
     if(ctx.selected != null) ctx.selected.removeClass("row-selected");
     $(e.currentTarget).addClass("row-selected");
     ctx.selected = $(e.currentTarget);
+  },
+
+  addRow : function(data)
+  {
+    if(data.length != this.attrCount) throw new Error("Mismatch of attributes.");
+    var html = "<tr class=\"row-body\">";
+    for(var i=0; i<this.attrCount; i++) {
+      html += "<td>"+data[i]+"</td>";
+    }
+    html += "</tr>";
+    $("#"+this.id+" tbody").append(html);
   }
 }
 
